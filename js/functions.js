7c3eea920afda3d436ecbddeb7901b30f967d7d3
Vinicius Oliveira <106452084+OliverSantoss@users.noreply.github.com>
2023-07-24 15:57:10 -0400

    Adição do menu scroll

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,12 @@ $(function () {
     mobile.click(function () {
         $(this).find('ul').slideToggle();
     });
+
+    $('nav a').click(function () {
+        let href = $(this).attr('href');
+        let offsetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offsetTop});
+        return false;
+    });
 });
