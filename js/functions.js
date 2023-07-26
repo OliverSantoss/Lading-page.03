ff332a861aeda371d326b6910e36b34e73a1d6f3
Vinicius Oliveira <106452084+OliverSantoss@users.noreply.github.com>
2023-07-26 15:23:31 -0400

    alterações de rotina

--- js/functions.js
+++ js/functions.js
@@ -11,4 +11,44 @@ $(function () {
         $('html,body').animate({'scrollTop':offsetTop});
         return false;
     });
+
+    var qta;
+    var delay = 3000;
+    var curIndex;
+
+    initSlider();
+    autoPlay();
+
+    function initSlider() {
+        qta = $('.sobre-author').length;
+        let sizeContainer = 100 * qta;
+        let sizeBoxSingle = 100 / qta;
+
+        $('.sobre-author').css('width', sizeBoxSingle + '%');
+        $('.scroll-wraper').css('width', sizeContainer +'%');
+
+        for (var i = 0; i < qta; i++){
+            if (i == 0) {
+                $('.slider-bullets').append('<span style="background-color: rgb(150,150,150);"></span>');
+            } else {
+                $('.slider-bullets').append('<span></span>');
+            }
+        }   
+    }
+
+    function autoPlay() {
+        setInterval(() => {
+            curIndex++;
+            if (curIndex == qta) {
+                curIndex = 0;
+                goToSlider(curIndex);
+            }
+        },delay);
+
+        function goToSlider(curIndex) {
+            var offsetX = $('.sobre-author').eq(curIndex).offset().left();
+            alert(offsetX);
+            $('.scrollEquipe').animate({ 'scrollleft': offsetX });
+        }
+    }
 });
